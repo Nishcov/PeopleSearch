be8bf5dffc515c8debb36820f31b055e1e508cf2
Eric McNamara <Nishcov@gmail.com>
2018-05-12 23:59:44 -0600

    Added Person model and sample PeopleController (to be modified going forward)

---
+++ PeopleSearch/Controllers/PeopleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PeopleSearch.Models;
+
+namespace PeopleSearch.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People/Random
+        public ActionResult Random()
+        {
+            Person peep = new Person() { Name = "Ro" };
+
+            return View(peep);
+        }
+    }
+}
---
+++ PeopleSearch/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PeopleSearch.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string[] Address { get; set; }
+        public int Age { get; set; }
+        public string[] Interests { get; set; }
+        public System.Drawing.Image Picture { get; set; }
+    }
+}
